a8efcdb6663ff47037714f44a0b24c1e2f7b9af6
CloudCat150 <cloudcat150@gmail.com>
2025-07-31 13:40:41 +0900

    Refactor : 디랙토리 구조 수정

--- client/app/src/main/java/com/example/camerastream/network/ApiService.kt
+++
@@ -1,8 +0,0 @@
-interface ApiService {
-    @Multipart
-    @POST("upload_frame")
-    fun sendFrame(@Part frame: MultipartBody.Part): Call<ResponseBody>
-
-    @GET("get_mp3")
-    fun getMp3(): Call<ResponseBody>
-}
--- client/app/src/main/java/com/example/camerastream/network/RetrofitClient.kt
+++
@@ -1,8 +0,0 @@
-object RetrofitClient {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl("http://<서버IP>:5000/")  // 로컬 서버 주소
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    val api: ApiService = retrofit.create(ApiService::class.java)
-}
